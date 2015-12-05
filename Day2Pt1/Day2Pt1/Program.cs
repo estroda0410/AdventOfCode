06fabe68995020fd727a3e85791bc22361fe1768
estro0410 <erin.stroda@gmail.com>
2015-12-05 12:52:44 -0800

    Adds StreamReader

--- Day2Pt1/Day2Pt1/Program.cs
+++ Day2Pt1/Day2Pt1/Program.cs
@@ -15,6 +15,31 @@ namespace Day2Pt1
 
             Console.WriteLine("Contents of txt = {0}", text);
 
+
+
+
+
+
+            int counter = 0;
+            string line;
+
+            System.IO.StreamReader file = new System.IO.StreamReader(@"C:\Users\Erin\Desktop\Git\AdventOfCode\Day2Pt1\Day2Input.txt");
+
+            while ((line = file.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+                counter++;
+            }
+
+
+
+
+
+
+
+
+
+
             Console.ReadLine();
         }
     }
